cd024d85488be1fcf6f6411575bb66680f189f2d
Nakshatra Tondepu <ntondepu@purdue.edu>
2025-05-20 20:30:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from qiskit import QuantumCircuit, execute, transpile
+from qiskit import QuantumCircuit, transpile
+from qiskit.utils import execute
 from qiskit.providers.aer import Aer
 from qiskit.visualization import plot_bloch_vector
 from qiskit.quantum_info import partial_trace, DensityMatrix
